30b52c382969ad8d4d925bf108a552faa7fdef7e
Uzairahmad14 <96127949+Uzairahmad14@users.noreply.github.com>
2023-04-03 20:16:04 +0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,12 +1,7 @@
-class Main {
-
+public class Main {
   public static void main(String[] args) {
-    
-    int first = 10;
-    int second = 20;
-
-    // add two numbers
-    int sum = first + second;
-    System.out.println(first + " + " + second + " = "  + sum);
+    for (int i = 0; i < 5; i++) {
+      System.out.println(i);
+    }  
   }
 }
